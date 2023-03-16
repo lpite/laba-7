b7e82db36bbbe77f220b3dc098193033724ebd3b
lpite <farionsasa@gmail.com>
2023-03-16 13:56:03 +0200

    2 zav

--- zav2.c
+++ zav2.c
@@ -3,37 +3,37 @@
 #include <stdio.h>
 
 int main() {
-	char str[] = "121 553 2342 551 11511 2332 2335 11 1";
-	int slen = sizeof(str) / sizeof(char),
-		thisWordLength=0;
-	for (int i = 0; i < slen; i++)
-	{		
-		char el = str[i];
-		if (el == 32 || slen == i + 1)
-		{
-			int wordStart = i - thisWordLength;
-			int isPalindrome = 1;
-			int bandera = thisWordLength % 2 == 0 ? wordStart + (thisWordLength) / 2 : wordStart + thisWordLength;
-			for (int i2 = wordStart; i2 < bandera; i2++) {
-				int rever = ((wordStart + thisWordLength) - i2 - 1) + wordStart;
-				if (str[i2] != str[rever])
-				{
-					isPalindrome = 0;
-				}
-			}
-			if (isPalindrome)
-			{
-				puts("palindrom");
-			}
-			else {
-				puts("not palindrom");
-			}
-			thisWordLength =0;
-		}
-		else {
-			thisWordLength++;
-		}
-	}
-	
-	return 0;
+  char str[] = "12345 64564 2134 756 11";
+  int slen = sizeof(str) / sizeof(char), thisWordLength = 0;
+  int showMessage = 1;
+  for (int i = 0; i < slen; i++) {
+    char el = str[i];
+    if (el == 32 || slen == i + 1) {
+      int wordStart = i - thisWordLength;
+      int isPalindrome = 1;
+      int bandera = thisWordLength % 2 == 0 ? wordStart + (thisWordLength) / 2
+                                            : wordStart + thisWordLength;
+      for (int i2 = wordStart; i2 < bandera; i2++) {
+        int rever = ((wordStart + thisWordLength) - i2 - 1) + wordStart;
+        if (str[i2] != str[rever]) {
+          isPalindrome = 0;
+        }
+      }
+      if (isPalindrome) {
+        for (int i2 = wordStart; i2 < wordStart + thisWordLength; i2++) {
+        	printf("%c",str[i2]);
+        }
+        showMessage = 0;
+      	puts("");
+      }
+      thisWordLength = 0;
+    } else {
+      thisWordLength++;
+    }
+  }
+  if (showMessage)
+  {
+  	puts("no palindrome nums");
+  }
+  return 0;
 }
